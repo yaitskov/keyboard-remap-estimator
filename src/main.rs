a650c7b44685eb86251abd71922385024d203407
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-18 17:33:02 +0200

    count cost

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,77 @@
+extern crate regex;
+
+use regex::Regex;
 use std::collections::HashMap;
 use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
 use std::path::Path;
+
+#[macro_use]
+extern crate lazy_static;
+
+type CharCountMap = HashMap<char, u32>;
 
 const ORIGIN_2_KEY_CHARS: &'static str = "(){}_\"@";
 const NEW_2_KEY_CHARS: &'static str = "90[]2'";
 
 fn main() {
-    let mut count: HashMap<char, i32> = HashMap::new();
+    let pattern = Regex::new(r"^.*[.]java$").unwrap();
+    let mut count: CharCountMap = HashMap::new();
     for file_path in env::args() {
-        analyze(&mut count, Path::new(file_path.as_str()));
+        count_chars(&pattern, &mut count, Path::new(file_path.as_str()));
+    }
+    let origin_cost = calc_cost(ORIGIN_2_KEY_CHARS, &count);
+    let new_cost = calc_cost(NEW_2_KEY_CHARS, &count);
+    println!("User pressed {} keys, but he could press {} keys",
+             origin_cost, new_cost);
+    if new_cost < origin_cost {
+        println!("Profit is {} keys less", origin_cost - new_cost)
     }
 }
 
-fn analyze(count: &mut HashMap<char, i32>, path: &Path) {
-    if path.is_file() {
-        println!("Analyze a regular file {}", path.display());
+fn calc_cost(two_key_chars: &str, count: &CharCountMap) -> u32 {
+    let mut cost = 0;
+    for (c, n) in count {
+        cost += n * if two_key_chars.chars().any(|a| a == *c) {
+            2
+        } else {
+            1
+        }
+    }
+    cost
+}
+
+fn count_chars(pattern: &Regex, count: &mut CharCountMap, path: &Path) {
+    if path.is_file() && pattern.is_match(path.file_name().unwrap().to_str().unwrap()) {
+        println!("Count chars in a regular file {}", path.display());
+        let mut file = match File::open(path) {
+            Err(why) => panic!("Couldn't open file {}: {}",
+                               path.display(), why.description()),
+            Ok(file) => file
+        };
+        let mut content = String::new();
+        match file.read_to_string(&mut content) {
+            Err(why) => {
+                println!("Couldn't read file [{}]: {}",
+                         path.display(), why.description());
+                return
+            }
+            Ok(_) => {}
+        }
+        for c in content.chars() {
+            if count.contains_key(&c) {
+                let n = count[&c];
+                count.insert(c, n + 1);
+            } else {
+                count.insert(c, 1);
+            }
+        }
     } else if path.is_dir() {
-        println!("Analyze dir {}", path.display());
+        println!("Count chars in dir {}", path.display());
         for entry in path.read_dir().unwrap() {
-            analyze(count, entry.unwrap().path().as_path())
+            count_chars(pattern, count, entry.unwrap().path().as_path())
         }
     } else {
         println!("Ignore path {} due not a regular file nor dir", path.display())
